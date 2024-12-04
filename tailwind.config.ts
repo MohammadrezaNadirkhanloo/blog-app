bf3686b5f177169e6db53bd8c8911fd35b1bc186
khanloo <m.nadirkhanloo1380@gmail.com>
2024-12-04 19:22:12 +0330

    Feat : add colos theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { light, dark } from "daisyui/src/theming/themes";
+
 
 export default {
-  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
@@ -21,7 +23,26 @@ export default {
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        light: {
+          ...light,
+          primary: "#2FCEF0",
+          secondary: "#1E3D53",
+          accent: "#162B3D",
+          gray: "#131E2A",
+          payne: "#395A6D",
+        },
+        dark: {
+          ...dark,
+          primary: "#2FCEF0",
+          secondary: "#1E3D53",
+          accent: "#162B3D",
+          gray: "#131E2A",
+          payne: "#395A6D",
+        },
+      },
+    ],
   },
   darkMode: ["class", '[data-theme="dark"]'],
 } satisfies Config;
