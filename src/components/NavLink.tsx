216dcf74bc8d5a95e9eff9b0afb564a0eaa0c9a9
khanloo <m.nadirkhanloo1380@gmail.com>
2024-12-04 19:23:27 +0330

    Update : add aptions navlink

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -6,15 +6,17 @@ import { usePathname } from "next/navigation";
 interface Props {
   path: string;
   children: string;
+  classStyle?: string;
 }
 
-function NavLink({ path, children }: Props) {
+function NavLink({ path, children, classStyle }: Props) {
   const pathname = usePathname();
 
   return (
     <Link
       className={`block py-2 hover:text-secondary-900 transition-all ease-out
         ${pathname === path ? "text-primary" : ""}
+        ${classStyle}
       `}
       href={path}
     >
