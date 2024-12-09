d10233d7670470642f2ecedbe39bfd070455af39
khanloo <m.nadirkhanloo1380@gmail.com>
2024-12-09 19:26:37 +0330

    Feat : creat component input form

---
+++ src/components/RHFTextField.tsx
@@ -0,0 +1,59 @@
+interface Props {
+  type?: string;
+  label: string;
+  name: string;
+  dir?: string;
+  register: any;
+  errors: any;
+  isRequired?: boolean;
+  validationSchema?: object;
+}
+
+const RHFTextField: React.FC<Props> = ({
+  type = "text",
+  label,
+  name,
+  dir = "rtl",
+  register,
+  errors,
+  isRequired,
+  validationSchema = {},
+  ...rest
+}) => {
+  const errorMessages = errors?.[name];
+  const hasError = !!(errors && errorMessages);
+  return (
+    <div className={`textField  `}>
+      <label className="form-control">
+        <div className="label">
+          <span className="label-text">
+            {label}
+            {isRequired && <span className="text-error">*</span>}
+          </span>
+        </div>
+        <input
+          autoComplete="off"
+          type={type}
+          id={name}
+          dir={dir}
+          {...register(name, validationSchema)}
+          {...rest}
+          className={`input input-primary input-bordered w-full shadow-md ${
+            hasError ? "border-error" : ""
+          }`}
+        />
+        <div className="label">
+          <span className="label-text-alt">
+            {errors && errors[name] && (
+              <span className="text-red-600 block text-xs mt-2">
+                {errors[name]?.message}
+              </span>
+            )}
+          </span>
+        </div>
+      </label>
+    </div>
+  );
+};
+
+export default RHFTextField;
