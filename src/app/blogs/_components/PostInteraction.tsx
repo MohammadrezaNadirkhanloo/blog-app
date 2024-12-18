71b19995bcd65cf8d3ea711bd2ae4cdd8f6a2872
khanloo <m.nadirkhanloo1380@gmail.com>
2024-12-18 10:50:49 +0330

    Feat : add like and bookmark

--- src/app/blogs/_components/PostInteraction.tsx
+++ src/app/blogs/_components/PostInteraction.tsx
@@ -6,38 +6,44 @@ import { CiBookmark } from "react-icons/ci";
 import { FaBookmark } from "react-icons/fa";
 import { GoHeart, GoHeartFill } from "react-icons/go";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
-
-// import { bookmarkPostApi, likePostApi } from "@/services/postServices";
-// import ButtonIcon from "@/ui/ButtonIcon";
-// import { toPersianDigits } from "@/utils/numberFormatter";
+import { bookmarkPostApi, likePostApi } from "@/services/postService";
 import { Post } from "@/utils/types";
-
-// import { useRouter } from "next/navigation";
-
-// import toast from "react-hot-toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 function PostInteraction({ commentsCount, _id, isLiked, isBookmarked }: Post) {
-  // const router = useRouter();
+  const router = useRouter();
 
-  // const likeHandler = async (postId) => {
-  //   try {
-  //     const { message } = await likePostApi(postId);
-  //     toast.success(message);
-  //     router.refresh();
-  //   } catch (error) {
-  //     toast.error(error?.response?.data?.message);
-  //   }
-  // };
+  const likeHandler = async (postId: string) => {
+    try {
+      const { message } = await likePostApi(postId);
+      toast.success(message);
+      router.refresh();
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const errorMsg = error.response?.data?.message || "An error occurred";
+        toast.error(errorMsg);
+      } else {
+        console.error("An unexpected error occurred:", error);
+      }
+    }
+  };
 
-  // const bookmarkHandler = async (postId) => {
-  //   try {
-  //     const { message } = await bookmarkPostApi(postId);
-  //     toast.success(message);
-  //     router.refresh();
-  //   } catch (error) {
-  //     toast.error(error?.response?.data?.message);
-  //   }
-  // };
+  const bookmarkHandler = async (postId: string) => {
+    try {
+      const { message } = await bookmarkPostApi(postId);
+      toast.success(message);
+      router.refresh();
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const errorMsg = error.response?.data?.message || "An error occurred";
+        toast.error(errorMsg);
+      } else {
+        console.error("An unexpected error occurred:", error);
+      }
+    }
+  };
 
   return (
     <div className="flex items-center border-gray-500 border-t-2 rounded-b-2xl rounded-t-none">
@@ -45,10 +51,10 @@ function PostInteraction({ commentsCount, _id, isLiked, isBookmarked }: Post) {
         <IoChatbubbleEllipsesOutline />
         <span>{toPersianDigits(commentsCount)}</span>
       </ButtonIcon>
-      <ButtonIcon variant="red">
+      <ButtonIcon variant="red" onClick={() => likeHandler(_id)}>
         {isLiked ? <GoHeartFill /> : <GoHeart />}
       </ButtonIcon>
-      <ButtonIcon variant="primary">
+      <ButtonIcon variant="primary" onClick={() => bookmarkHandler(_id)}>
         {isBookmarked ? <FaBookmark /> : <CiBookmark />}
       </ButtonIcon>
     </div>
